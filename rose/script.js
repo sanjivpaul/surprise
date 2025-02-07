ad76f57d5eb1c139d8a4c01bf9f4014746fcdeb2
Sanjiv Paul <paulsanjiv81@gmail.com>
2025-02-07 23:43:12 +0530

    rose

---
+++ rose/script.js
@@ -0,0 +1,4 @@
+// This script is not strictly necessary, but you can add interactivity with JS if desired
+document.querySelector(".rose").addEventListener("click", () => {
+  alert("You clicked the rose!");
+});
